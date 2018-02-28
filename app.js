00e0c0b4e218dbadf28ba0876b05bdf3a3f6384b
Albert Jose <albertjose.au@gmail.com>
2018-02-28 07:47:26 +0000

    0x02 - Add views, Update .instructions,app.js

--- app.js
+++ app.js
@@ -4,11 +4,38 @@ var mongoose = require("mongoose");
 
 var app=express();
 
-mongoose.connect("mongodb://localhost/db_wordoftheday")
+// app config
+mongoose.connect(process.env.DATABASEURL);
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(process.env.PORT,process.env.IP,function(){
-    console.log("wordoftheday server is RUNNING !!!")
+// mongoose model config
+var wordSchema = new mongoose.Schema({
+    title: {type: String, unique: true},
+    description: String,
+    created: {type: Date, default:Date.now}
 });
+
+var Word = mongoose.model("Word",wordSchema);
+
+// // test data
+// Word.create({
+//     title: "word",
+//     description: "A single distinct meaningful element of speech or writing, used with others (or sometimes alone) to form a sentence and typically shown with a space on either side when written or printed."
+// });
+
+//RESTful routes
+app.get("/", function(req,res){
+    res.redirect("/words");
+});
+
+app.get("/words",function(req,res){
+    res.render("index");
+});
+
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("wordoftheday server is RUNNING !!!");
+});
+
